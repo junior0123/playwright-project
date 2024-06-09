da98af9f3a82fc4a6540ed9816916825093c2e62
Alvaro Sivila <juniorsivila123@gmail.com>
2024-06-09 19:43:43 -0400

    Add additinal step for navigates through all results and its logic

--- features/steps/test_search_steps.py
+++ features/steps/test_search_steps.py
@@ -2,9 +2,12 @@ import pytest
 from pytest_bdd import scenarios, given, when, then, parsers
 from playwright.sync_api import expect
 from features.pages.home_page import HomePage
+from features.pages.job_information_page import JobInformationPage
 from features.pages.search_page import SearchPage
 from features.pages.login_page import LoginPage
 import os
+
+from utils.models import JobInformation
 
 scenarios(os.path.join(os.path.dirname(__file__), '../search_job.feature'))
 @pytest.fixture(scope="function")
@@ -14,6 +17,11 @@ def search_page(page):
 @pytest.fixture(scope="function")
 def home_page(page):
     return HomePage(page)
+
+@pytest.fixture(scope="function")
+def job_information_page(page):
+    return JobInformationPage(page)
+
 
 @when('the user go to the LinkedIn jobs search page')
 def navigate_to_search_jobs(search_page):
@@ -29,10 +37,14 @@ def search_for_job(search_page, job_title, location):
 @when(parsers.parse('the user selects the work mode "{mode}"'))
 def select_work_mode(search_page, mode):
     search_page.select_work_mode(mode)
+    #search_page.select_work_mode("Híbrido")
+    #search_page.select_work_mode("Presencial")
 
 @when(parsers.parse('the user selects the "{date}" date'))
 def select_publication_date(search_page, date):  # Nombre más descriptivo
     search_page.publication_date(date)
+    #search_page.publication_date("Mes pasado")
+    #search_page.publication_date("Semana pasada")
 
 @when('the user selects single application filter')
 def select_single_application(search_page):
@@ -43,6 +55,31 @@ def select_single_application(search_page):
 def verify_search_results(search_page):
     search_page.apply_filters()
 
+
 @when('the user opens the filter panel')
-def step_when(search_page):
+def open_filter(search_page):
     search_page.open_filter_panel()
+
+
+@then('the user navigates through all the results')
+@pytest.mark.usefixtures("db_session")
+def get_information(job_information_page, search_page, db_session):
+    exist_next_page = True
+    count = 2
+    while exist_next_page:
+        ids = search_page.get_links()
+        for job_id in ids:
+            search_page.click_on_job(job_id)
+            title = job_information_page.get_job_title()
+            content = job_information_page.get_job_content()
+            restriction = job_information_page.location_restriction_is_present()
+            url = f"https://www.linkedin.com/jobs/view/{job_id}"
+            post = JobInformation(job_id=job_id, title=title, url=url, restriction=restriction, compatible="unprocessed", description=content)
+            db_session.add(post)
+            db_session.commit()
+        if search_page.page_exists(count):
+            search_page.click_on_next_page(count)
+        else:
+            exist_next_page = False
+        count = count + 1
+
