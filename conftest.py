462eff8f9df09eef5c1e2e78ba1fb3f821fb3b23
Alvaro Sivila <juniorsivila123@gmail.com>
2024-06-09 19:39:03 -0400

    Delete unnecesary lines of code and decrease slow mo parameter

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ from pytest_bdd import given, then, when
 from utils.db_fixture import db_session
 from features.pages.login_page import LoginPage
 from utils.database import engine, Base
+
 load_dotenv()
 
 
@@ -15,34 +16,33 @@ load_dotenv()
 def playwright_context():
     try:
         with sync_playwright() as playwright:
-            browser = playwright.chromium.launch(args=['--start-maximized'], headless=False, slow_mo=250)
+            browser = playwright.chromium.launch(args=['--start-maximized'], headless=False, slow_mo=100)
             context = browser.new_context(no_viewport=True)  #browser window
             context.clear_cookies()
-            yield context #proporciona el contexto para otros test
+            yield context  #proporciona el contexto para otros test
             context.close()
             browser.close()
     except Exception as e:
         print(f"Error: {e}")
-        # Mantén el navegador abierto para inspección manual
-        #input("Presiona Enter para cerrar el navegador...")
+
 
 @pytest.fixture(scope="function")
 def page(playwright_context):
     try:
-        page = playwright_context.new_page() #tab window
-        yield page #proporciona la pagina a las funciones que lo necesiten
+        page = playwright_context.new_page()  #tab window
+        yield page  #proporciona la pagina a las funciones que lo necesiten
         page.close()
     except Exception as e:
         print(f"Error: {e}")
-        # Mantén el navegador abierto para inspección manual
+
 
 @pytest.fixture(scope='session', autouse=True)
 def setup_database():
-    # Inicializa la base de datos
     Base.metadata.create_all(bind=engine)
     yield
     # Limpia la base de datos después de los tests
     #Base.metadata.drop_all(bind=engine)
+
 
 @pytest.fixture(scope="function")
 def login_page(page):
@@ -64,6 +64,7 @@ def login_with_valid_credentials(login_page):
 @then('the user should be redirected to the dashboard')
 def verify_dashboard_redirect(login_page):
     login_page.verification_login_successfully()
+
 
 @then('the user logout')
 def logout_from_the_site(login_page):
