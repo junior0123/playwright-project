85600ee5cd03f67e62d48c0d9627f0a315db52fd
Alvaro Sivila <juniorsivila123@gmail.com>
2024-06-10 19:56:02 -0400

    Change state for dont have outputs in console of Sqlalcemy orm

--- utils/database.py
+++ utils/database.py
@@ -6,6 +6,6 @@ import os
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
