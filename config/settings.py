27192a536922261261495cd24f57862560625349
Alvaro Sivila <juniorsivila123@gmail.com>
2024-06-25 10:56:41 -0400

    Add file with user information

---
+++ config/settings.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    USER_INFORMATION = {
+        "role": "QA Engineer - QA Automation - QA Manual - Java Backend Developer - Python Backend Developer",
+        "technologies_used": ["Java", "Python", "JavaScript", "Flutter", "Assembly", "Selenium", "Cypress",
+                              "Playwright", "Jmeter",
+                              "Appium", "Postman", "SQL", "Git", "Github", "Jira", "Cucumber", "Gherkin", "Jenkins",
+                              "Pytest", "Junit", "TestNG"],
+        "skills": ["GUI Testing", "API Testing", "Mobile Testing", "Web Testing", "Testing methodologies",
+                   "Bug Life Cycle",
+                   "Agile Methodologies"],
+        "years_of_experience": "1 - 2 ",
+        "seniority": "Trainee - Junior - Mid Level",
+        "location": "Remote - Bolivia"
+    }
